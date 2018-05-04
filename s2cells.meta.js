e3feb48405a94156c36f1e48cd6e0fcb6d900bf3
Case Schmidt <caserschmidt@gmail.com>
2018-05-04 12:15:08 -0700

    Update and rename l17cells.meta.js to s2cells.meta.js

---
+++ s2cells.meta.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @id             iitc-plugin-s2-cells@vib
+// @name           IITC plugin: Show Configurable S2 Cells
+// @author         vib+	Dragonsangel+nikolawannabe
+// @category       Layer
+// @version        0.1.8
+// @namespace      https://github.com/nikolawannabe/l17cells
+// @updateURL      https://raw.githubusercontent.com/nikolawannabe/l17cells/master/s2-cells.meta.js
+// @downloadURL    https://github.com/nikolawannabe/l17cells/raw/master/s2-cells.user.js
+// @description    IITC: Shows configurable S2 level cells on the map
+// @include        https://*.ingress.com/intel*
+// @include        http://*.ingress.com/intel*
+// @match          https://*.ingress.com/intel*
+// @match          http://*.ingress.com/intel*
+// @include        https://*.ingress.com/mission/*
+// @include        http://*.ingress.com/mission/*
+// @match          https://*.ingress.com/mission/*
+// @match          http://*.ingress.com/mission/*
+// @grant          none
+// ==/UserScript==
